9598ad60e2d814ba91dbd6d9d655f1c1dd2b2e95
MadMoneyLab <119470128+MadMoneyLab@users.noreply.github.com>
2024-01-26 18:08:25 +0000

    updates

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Run the Flask app on https://192.168.12.140:5000
     app.run(debug=True)
